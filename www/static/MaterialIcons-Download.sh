5b16d7969bbfcdaca6eb0638d35bbc174568aa36
Tomaso Minelli <tomaso.minelli@unipd.it>
2017-04-20 11:42:57 +0200

    Initial commit

---
+++ www/static/MaterialIcons-Download.sh
@@ -0,0 +1,8 @@
+# https://github.com/google/material-design-icons/tree/master/iconfont
+
+wget https://github.com/google/material-design-icons/raw/master/iconfont/MaterialIcons-Regular.eot
+wget https://github.com/google/material-design-icons/raw/master/iconfont/MaterialIcons-Regular.ijmap
+wget https://github.com/google/material-design-icons/raw/master/iconfont/MaterialIcons-Regular.svg
+wget https://github.com/google/material-design-icons/raw/master/iconfont/MaterialIcons-Regular.ttf
+wget https://github.com/google/material-design-icons/raw/master/iconfont/MaterialIcons-Regular.woff
+wget https://github.com/google/material-design-icons/raw/master/iconfont/MaterialIcons-Regular.woff2
